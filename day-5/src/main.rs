f9d79c25036d8ebf5536bc21522707787fac0e2e
Corentin Bernard <corentin@lumapps.com>
2022-12-05 14:18:05 +0100

    format

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -29,10 +29,10 @@ fn main() {
         for line in lines {
             if let Ok(ip) = line {
                 if step_reg.is_match(&ip) {
-                    let caps = step_reg.captures(&ip).unwrap();
-                    let amount  = caps[1].parse::<usize>().unwrap();
-                    let from = caps[2].parse::<usize>().unwrap() - 1;
-                    let to   = caps[3].parse::<usize>().unwrap() - 1;
+                    let caps   = step_reg.captures(&ip).unwrap(),
+                    let amount = caps[1].parse::<usize>().unwrap();
+                    let from   = caps[2].parse::<usize>().unwrap() - 1;
+                    let to     = caps[3].parse::<usize>().unwrap() - 1;
 
 
                     // Solution 1
